76577786fb4e95540d4491f026d9ae468cfc5f4c
Chris <1995cbald@gmail.com>
2022-11-22 11:01:24 -0500

    allows for basic inputs with any number of operators Bugs to be fixed: integer limit, adding on to result field, decimals not computing correctly

--- java/java.js
+++ java/java.js
@@ -1,56 +1,100 @@
-function add(storage) {
-   let result = a + b;
-   return result
-}
 
-function subtract() {
-    let result = a - b;
-    return result
-}
-
-function mult(a, b){
-
-}
-
-function di(a, b){
-
-}
-
-let storage = [];
+let stor = '';
 
 const buttons = document.querySelectorAll('#button')
 result = "";
 
+
+
 buttons.forEach(button =>{
-    button.addEventListener('click', function(){ //you might need to make this a so that it ends at an equals sign
+    button.addEventListener('click', function(){ 
+
+      document.getElementById('screen').innerHTML += button.value;
     const currentHTML = document.getElementById('screen').innerHTML;
-    const nextHTML = ' ' + currentHTML;
-    document.getElementById('screen').innerHTML = nextHTML;
-    document.getElementById('screen').innerHTML += button.value;
-   storage.push(button.value) 
-        console.log(storage)
+     stor = ' ' + currentHTML;
+    document.getElementById('screen').innerHTML = stor;
+     })
     })
-})
 
-/*function showOld() {
-    alert('test');
-    }*/
-//alternatively, i might need to assign the value on click of operators to a function that gets exectuted when equal function is carried out
 
-//storage is an array. Need to loop through and find solutiosn accordingly
-let eq = document.querySelectorAll('#button2');
-function equal(storage) {
-    for (var i = 0; i < storage.length; i++) {
-       let total = storage.reduce(0);
+   /* function ple(value) { 
+      document.getElementById('screen').innerHTML += value;
+    const currentHTML = document.getElementById('screen').innerHTML;
+     stor = ' ' + currentHTML;
+    document.getElementById('screen').innerHTML = stor;
     }
-    
-    let solution = document.getElementById('screen').innerHTML;
+console.log(stor)*/
+
+//two paths forward: I need to find a way to keep stored value being erased with new clear button press or I need to create a new function to host under onlick for each of the inputs
+
+//why is using the button redefining the value of stor? Everything works when done solely through the console.
+
+function equal(stor) {
+  var expressionIndex = Math.max(stor.lastIndexOf("-"), stor.lastIndexOf("+"));
+  if (expressionIndex === -1) {
+    expressionIndex = Math.max(stor.lastIndexOf("*"), stor.lastIndexOf("/"));
+  }
+  if (expressionIndex === -1) {
+    var num = Number.parseInt(stor.trim());
+    if (isNaN(num)) {
+      throw Exception("not a valid number");
+    } else {
+      return num;
+    }
+  } else {
+    var leftVal = equal(stor.substring(0, expressionIndex).trim());
+    var rightVal = equal(stor.substring(expressionIndex + 1).trim());
+    switch (stor[expressionIndex]) {
+      case "+":
+        document.getElementById('screen').innerHTML = leftVal + rightVal;
+        return leftVal + rightVal;
+      case "-":
+        document.getElementById('screen').innerHTML = leftVal - rightVal;
+        return leftVal - rightVal;
+      case "*":
+        document.getElementById('screen').innerHTML = leftVal * rightVal;
+        return leftVal * rightVal;
+      case "/":
+        document.getElementById('screen').innerHTML = leftVal / rightVal;
+        return leftVal / rightVal;
+  
+    }
+  }
 }
 
-let but = document.querySelectorAll('#button1');
+
+function parse(stor) {
+  var expressionIndex = Math.max(stor.lastIndexOf("-"), stor.lastIndexOf("+"));
+  if (expressionIndex === -1) {
+    expressionIndex = Math.max(stor.lastIndexOf("*"), stor.lastIndexOf("/"));
+  }
+  if (expressionIndex === -1) {
+    var num = Number.parseInt(stor.trim());
+    if (isNaN(num)) {
+      throw Exception("not a valid number");
+    } else {
+      return { type: "number", value: num };
+    }
+  } else {
+    var leftNode = parse(stor.substring(0, expressionIndex).trim());
+    var rightNode = parse(stor.substring(expressionIndex + 1).trim());
+    return {
+      type: "expression",
+      value: stor[expressionIndex],
+      left: leftNode,
+      right: rightNode
+    };
+  }
+}
+
+    
+
+
+//clears current inner.HTML
 var og = document.getElementById("screen"); 
-var old = og.innerHTML;
+var old = og.innerHTML; 
 
 function clearI() {
    og.innerHTML = old;
+   stor = old;
 }
